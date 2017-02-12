829e8d795c9e1f76d594e2f847c446465a6b0f61
miquecg <git@miguelcasal.net>
2017-02-12 17:27:49 +0100

    Test inclusive ranges with more than two integers

--- test/math_ranges_tests.erl
+++ test/math_ranges_tests.erl
@@ -7,4 +7,6 @@ inclusive_limits_ranges_test_() ->
     ?_assertEqual({}, ranges:get_all_points(<<"[]">>)),
     ?_assertEqual({0}, ranges:get_all_points(<<"[0]">>)),
     ?_assertEqual({1}, ranges:get_all_points(<<"[1]">>)),
-    ?_assertEqual({1, 2}, ranges:get_all_points(<<"[1, 2]">>)).
+    ?_assertEqual({1, 2}, ranges:get_all_points(<<"[1, 2]">>)),
+    ?_assertEqual({3, 4, 5}, ranges:get_all_points(<<"[3, 5]">>)),
+    ?_assertEqual({2, 3, 4, 5, 6, 7}, ranges:get_all_points(<<"[2, 7]">>)).
